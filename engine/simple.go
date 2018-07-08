9b244343b5d087ce2014d43145213d0bc6ed3d72
liyifeng <lyf@soecode.com>
2018-07-08 23:46:03 +0800

    comment fetech url log

--- engine/simple.go
+++ engine/simple.go
@@ -31,7 +31,7 @@ func (e SimpleEngine) Run(seeds ... Request) {
 }
 
 func worker(r Request) (ParseResult, error) {
-	log.Printf("Fetching %s", r.Url)
+	//log.Printf("Fetching %s", r.Url)
 	body, err := fetcher.Fetch(r.Url)
 	if err != nil {
 		log.Printf("Fetcher: error fetching url %s: %v", r.Url, err)
