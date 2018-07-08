fa3c20cbb690c080c0c192b36488afd59934be49
liyifeng <lyf@soecode.com>
2018-07-08 23:46:55 +0800

    add more guess user

--- zhenai/parser/profile.go
+++ zhenai/parser/profile.go
@@ -19,6 +19,7 @@ var hukouRe = regexp.MustCompile(`<td><span class="label">籍贯：</span>([^<]+
 var xinzuoRe = regexp.MustCompile(`<td><span class="label">星座：</span><span field="">([^<]+)</span></td>`)
 var houseRe = regexp.MustCompile(`<td><span class="label">住房条件：</span><span field="">([^<]+)</span></td>`)
 var carRe = regexp.MustCompile(`<td><span class="label">是否购车：</span><span field="">([^<]+)</span></td>`)
+var guessRe = regexp.MustCompile(`<a class="exp-user-name"[^>]*href="(http://album.zhenai.com/u/[\d]+)">([^<]+)</a>`)
 
 func ParseProfile(contents []byte, name string) engine.ParseResult {
 	profile := model.Profile{
@@ -40,6 +41,18 @@ func ParseProfile(contents []byte, name string) engine.ParseResult {
 	result := engine.ParseResult{
 		Items: []interface{}{profile},
 	}
+
+	matchs := guessRe.FindAllSubmatch(contents, -1)
+	for _, m := range matchs {
+		name := string(m[2])
+		result.Requests = append(result.Requests, engine.Request{
+			Url: string(m[1]),
+			ParserFunc: func(c []byte) engine.ParseResult {
+				return ParseProfile(c, name)
+			},
+		})
+	}
+
 	return result
 }
 
