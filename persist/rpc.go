2f827977be0f65b6f7199f7536dd980c856cdbb3
liyifeng <lyf@soecode.com>
2018-07-23 00:07:58 +0800

    update ItemSaverService

--- persist/rpc.go
+++ persist/rpc.go
@@ -15,12 +15,11 @@ type ItemSaverService struct {
 
 func (s *ItemSaverService) Save(item engine.Item, result *string) error {
 	err := Save(s.Client, s.Index, item)
-	if err != nil {
+	if err == nil {
+		log.Printf("Item %v saved.", item)
+		*result = "ok"
+	} else {
 		log.Printf("Error saving item %v: %v", item, err)
-		return err
 	}
-
-	log.Printf("Item %v saved.", item)
-	*result = "ok"
-	return nil
+	return err
 }
